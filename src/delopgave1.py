957951b46b6482bf22d322620c208eca53c01d39
Emilie <emilie.vistisen@specialisterne.dk>
2026-02-25 09:49:08 +0100

    ekstraopgave er basalt set færdig

--- src/delopgave1.py
+++ src/delopgave1.py
@@ -2,19 +2,20 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from wordcloud import WordCloud
 
 
-parent_dir = Path.cwd().parent
-namelist_filepath = parent_dir / "data/Navneliste.txt"
+data_dir = Path.cwd().parent / 'data'
+namelist_file = 'Navneliste.txt'
 
-with open(namelist_filepath, 'r') as f:
+with open(data_dir / namelist_file, 'r') as f:
     lines = f.readlines()
 
 # sort and print names
 def sort_and_print_names(names: list[str]) -> None: 
     names_sorted = sorted(names)
     names_sorted = sorted(names_sorted, key=len)
-    print(names_sorted)
+    print(f'Names sorted:\n {names_sorted}')
 
 names = lines[0].split(',')
 #sort_and_print_names(names)
@@ -30,14 +31,38 @@ for letter in list(letters):
 
 # Extension to exercise 1
 
-# Barplot
-fig, ax = plt.subplots()
-df = pd.DataFrame(list(letter_frequency.items()), columns=['Letter', 'Frequency'])
-df = df.sort_values(by="Letter")
+# Letter frequency analysis
+
+fig, ax = plt.subplots(1, 2, figsize=(12,6))
+
+letter_df = pd.DataFrame(list(letter_frequency.items()), columns=['Letter', 'Frequency'])
+letter_df = letter_df.sort_values(by="Letter")
 vowels = 'aeiouy'
-colors = ['orange' if l in vowels else 'darkgrey' for l in df.Letter]
-print(colors)
-sns.barplot(x=df.Letter, y=df.Frequency, palette=colors, ax=ax)
+letter_colors = ['orange' if l in vowels else 'darkgrey' for l in letter_df.Letter]
+sns.barplot(x=letter_df.Letter, y=letter_df.Frequency, hue=letter_df.Letter, palette=letter_colors, ax=ax[0])
+
+
+wordcloud = WordCloud(width=480, height=480, margin=0).fit_words(letter_frequency)
+ax[1].imshow(wordcloud, interpolation='bilinear')
+ax[1].set_axis_off()
+#ax[1].margins(x=0, y=0)
+
 plt.show()
 
+
+# Name length analysis
+
+names_df = pd.DataFrame({'Name': names, 'Name length': [len(name) for name in names]})
+
+fig, ax = plt.subplots()
+sns.histplot(x=names_df['Name length'], discrete=True, edgecolor='white', color='cornflowerblue', ax=ax)
+ax.set_xticks([3,4,5,6,7,8,9,10,11])
+mean_and_median = f'''
+Mean:     {names_df['Name length'].mean():.2f}
+Median:  {names_df['Name length'].median():.2f}
+Std:        {names_df['Name length'].std():.2f}
+'''
+ax.text(9, 240, mean_and_median, fontsize=11)
+
+plt.show()
 
